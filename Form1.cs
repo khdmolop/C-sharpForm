346dcf90d6ac3af081c3fc9ddd31472792d85709
khdmolop <83680629+khdmolop@users.noreply.github.com>
2021-11-26 21:55:38 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Left += 50;
+            this.Top += 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Left -= 50;
+            this.Top -= 10;
+        }
+    }
+}
